152c1a1dc833a28f7dcc60b378204df45505b68b
taufik-hdyt <taufikhdyt2599@gmail.com>
2024-01-05 05:24:28 +0700

    updated

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("My App"),
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Text("Hay"),
+        ),
+      ),
+    );
+  }
+}
