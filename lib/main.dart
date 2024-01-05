1ddba817b58d4733c3e5c0ef3933018c0e8f9460
taufik-hdyt <taufikhdyt2599@gmail.com>
2024-01-05 13:32:04 +0700

    belajar list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,57 @@
 import 'package:flutter/material.dart';
 
-main() {
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final List<Color> myColor = [
+    Colors.red,
+    Colors.yellow,
+    Colors.green,
+    Colors.blue
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Colum"),
-          centerTitle: true,
+          title: const Text("List View"),
           backgroundColor: Colors.blue,
         ),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: 200,
-                height: 50,
-                color: Colors.green,
-              ),
-              Container(
-                width: 100,
-                height: 50,
-                color: Colors.blue,
-              ),
-              Container(
-                width: 50,
-                height: 50,
-                color: Colors.amber,
-              ),
-            ]),
+        // menambahkan list view
+        // body: ListView.builder(
+        //   itemCount: myColor.length,
+        //   itemBuilder: (context, index) {
+        //     return Container(
+        //       height: 300,
+        //       width: 300,
+        //       color: myColor[index],
+        //     );
+        //   },
+        // ),
+
+        // menambahkan spasi dan warna spasinya
+        body: ListView.separated(
+          separatorBuilder: (context, index) {
+            return Container(
+              height: 10,
+              color: Colors.black,
+            );
+          },
+          itemCount: myColor.length,
+          itemBuilder: (context, index) {
+            // Mengembalikan return Container =============
+            // return Container(
+            //   height: 300,
+            //   width: 300,
+            //   color: myColor[index],
+            // );
+
+            // Mengembalikan return Text =============
+            return Text("Hallo");
+          },
+        ),
       ),
     );
   }
