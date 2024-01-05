e37906325ba83c93aa8b67e2b52aecf1ada0ae98
taufik-hdyt <taufikhdyt2599@gmail.com>
2024-01-06 06:49:01 +0700

    drag and drop

--- lib/main.dart
+++ lib/main.dart
@@ -12,31 +12,55 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String message = "ini adalah text";
-
+  Color color1 = Colors.blue;
+  Color color2 = Colors.yellow;
+  Color targetColor = Colors.green;
+  bool isAccepted = false;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("TextAnonymous"),
+          title: Text("Drag and Drop"),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(message),
-              TextButton(
-                  onPressed: () {
-                    setState(() {
-                      message = "Tombol sudah di tekan";
-                    });
-                  },
-                  child: Text(
-                    "Tekan saya",
-                  ))
-            ],
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Draggable<Color>(
+                  data: color1,
+                  child: SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Material(
+                      color: color1,
+                      shape: StadiumBorder(),
+                    ),
+                  ),
+                  childWhenDragging: SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Material(
+                      color: Colors.black12,
+                      shape: StadiumBorder(),
+                      elevation: 3,
+                    ),
+                  ),
+                  feedback: SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Material(
+                      color: Colors.blue,
+                      shape: StadiumBorder(),
+                      elevation: 3,
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
