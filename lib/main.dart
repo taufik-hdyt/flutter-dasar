e7de4ec3d8f9336c7a14cb443bdcadb7f97cb51f
taufik-hdyt <taufikhdyt2599@gmail.com>
2024-01-06 11:05:31 +0700

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -5,34 +5,140 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: AppBar(
-        leading: const Icon(
-          Icons.adb,
-          color: Colors.white,
-        ),
-        title: const Text(
-          "AppBar",
-          style: TextStyle(color: Colors.white),
-        ),
-        actions: const <Widget>[
-          IconButton(
-            onPressed: null,
-            icon: Icon(Icons.settings),
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  double getSmallDiameter(BuildContext context) =>
+      MediaQuery.of(context).size.width * 2 / 3;
+  double getBigDiameter(BuildContext context) =>
+      MediaQuery.of(context).size.width * 7 / 8;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            right: -getSmallDiameter(context) / 3,
+            top: -getSmallDiameter(context) / 3,
+            child: Container(
+              width: getSmallDiameter(context),
+              height: getSmallDiameter(context),
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                      colors: [Color(0xffb226b2), Color(0xffff6da7)],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter)),
+            ),
           ),
-          IconButton(onPressed: null, icon: Icon(Icons.agriculture_rounded)),
+          Positioned(
+            left: -getSmallDiameter(context) / 4,
+            top: -getSmallDiameter(context) / 4,
+            child: Container(
+              width: getBigDiameter(context),
+              height: getBigDiameter(context),
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                      colors: [Color(0xffb226b2), Color(0xffff4891)],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter)),
+              child: const Center(
+                child: Text(
+                  "Dribble",
+                  style: TextStyle(color: Colors.white, fontSize: 30),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            right: -getBigDiameter(context) / 2,
+            bottom: -getBigDiameter(context) / 2,
+            child: Container(
+              width: getBigDiameter(context),
+              height: getBigDiameter(context),
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle, color: Colors.blue),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(5)),
+                  margin: EdgeInsets.fromLTRB(20, 300, 20, 10),
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 25),
+                  child: const Column(children: [
+                    TextField(
+                      decoration: InputDecoration(
+                          icon: Icon(
+                            Icons.email,
+                            color: Colors.blue,
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.blue),
+                          ),
+                          labelStyle: TextStyle(color: Colors.green),
+                          labelText: "Email"),
+                    ),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          icon: Icon(
+                            Icons.vpn_key,
+                            color: Colors.blue,
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.blue),
+                          ),
+                          labelStyle: TextStyle(color: Colors.green),
+                          labelText: "Password"),
+                    )
+                  ]),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(0, 0, 20, 20),
+                    child: Text(
+                      "Forgot Password ?",
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 0, 20, 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        height: 40,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                  colors: [Colors.blue, Colors.green])),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
         ],
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                  colors: [Color(0xff0096ff), Color(0xff6610f2)],
-                  begin: FractionalOffset.topLeft,
-                  end: FractionalOffset.topRight)),
-        ),
       ),
     );
   }
