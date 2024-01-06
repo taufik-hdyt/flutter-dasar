030b7a11c7a510587dad633a6f96d9399942b979
taufik-hdyt <taufikhdyt2599@gmail.com>
2024-01-06 11:18:04 +0700

    tab bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,144 +1,52 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
-    );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  double getSmallDiameter(BuildContext context) =>
-      MediaQuery.of(context).size.width * 2 / 3;
-  double getBigDiameter(BuildContext context) =>
-      MediaQuery.of(context).size.width * 7 / 8;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey,
-      body: Stack(
-        children: <Widget>[
-          Positioned(
-            right: -getSmallDiameter(context) / 3,
-            top: -getSmallDiameter(context) / 3,
-            child: Container(
-              width: getSmallDiameter(context),
-              height: getSmallDiameter(context),
-              decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: LinearGradient(
-                      colors: [Color(0xffb226b2), Color(0xffff6da7)],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter)),
-            ),
-          ),
-          Positioned(
-            left: -getSmallDiameter(context) / 4,
-            top: -getSmallDiameter(context) / 4,
-            child: Container(
-              width: getBigDiameter(context),
-              height: getBigDiameter(context),
-              decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: LinearGradient(
-                      colors: [Color(0xffb226b2), Color(0xffff4891)],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter)),
-              child: const Center(
-                child: Text(
-                  "Dribble",
-                  style: TextStyle(color: Colors.white, fontSize: 30),
-                ),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text("Tab Bar"),
+            bottom: const TabBar(tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.chat),
+                text: "Chat",
               ),
-            ),
+              Tab(
+                icon: Icon(Icons.comment),
+                text: "Comments",
+              ),
+              Tab(
+                icon: Icon(Icons.delete),
+                text: "Delete",
+              ),
+            ]),
           ),
-          Positioned(
-            right: -getBigDiameter(context) / 2,
-            bottom: -getBigDiameter(context) / 2,
-            child: Container(
-              width: getBigDiameter(context),
-              height: getBigDiameter(context),
-              decoration: const BoxDecoration(
-                  shape: BoxShape.circle, color: Colors.blue),
-            ),
+          body: const TabBarView(
+            children: <Widget>[
+              Center(
+                child: Text("Tab satu"),
+              ),
+              Center(
+                child: Text("Tab dua"),
+              ),
+              Center(
+                child: Text("Tab 3"),
+              ),
+              Center(
+                child: Text("Tab 4"),
+              ),
+            ],
           ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: ListView(
-              children: <Widget>[
-                Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(color: Colors.white),
-                      borderRadius: BorderRadius.circular(5)),
-                  margin: EdgeInsets.fromLTRB(20, 300, 20, 10),
-                  padding: EdgeInsets.fromLTRB(10, 0, 10, 25),
-                  child: const Column(children: [
-                    TextField(
-                      decoration: InputDecoration(
-                          icon: Icon(
-                            Icons.email,
-                            color: Colors.blue,
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.blue),
-                          ),
-                          labelStyle: TextStyle(color: Colors.green),
-                          labelText: "Email"),
-                    ),
-                    TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          icon: Icon(
-                            Icons.vpn_key,
-                            color: Colors.blue,
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.blue),
-                          ),
-                          labelStyle: TextStyle(color: Colors.green),
-                          labelText: "Password"),
-                    )
-                  ]),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Container(
-                    margin: EdgeInsets.fromLTRB(0, 0, 20, 20),
-                    child: Text(
-                      "Forgot Password ?",
-                      style: TextStyle(color: Colors.white, fontSize: 15),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(0, 0, 20, 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.5,
-                        height: 40,
-                        child: Container(
-                          decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                  colors: [Colors.blue, Colors.green])),
-                        ),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
